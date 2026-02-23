c128bef9e48078cad5cf1b5d34c52f448a879e25
paulgri <paulgrishmanovsky@gmail.com>
2026-02-23 22:16:43 +0500

    RA-02 done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.paulgri.ra1app"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
