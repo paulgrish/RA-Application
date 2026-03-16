f1fe2736f3ee05c5e3bab918a0f6fbf5ef240547
paulgri <paulgrishmanovsky@gmail.com>
2026-03-16 23:24:47 +0500

    RA-03 binding

--- app/src/main/java/ru/paulgri/ra1app/MainActivity.kt
+++ app/src/main/java/ru/paulgri/ra1app/MainActivity.kt
@@ -2,12 +2,20 @@ package ru.paulgri.ra1app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.paulgri.ra1app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding: ActivityMainBinding
+        get() = _binding ?: throw Exception()
+
+    private var _binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
