80c67fbacfa5c581c235b8a31a3691a893e423b5
paulgri <paulgrishmanovsky@gmail.com>
2026-04-14 00:18:22 +0500

    RA-05 fix

--- app/src/main/java/ru/paulgri/ra1app/CategoriesListFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/CategoriesListFragment.kt
@@ -18,22 +18,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CategoriesListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    val binding: FragmentListCategoriesBinding
+    private val binding: FragmentListCategoriesBinding
         get() = _binding ?: throw IllegalStateException("CategoriesListFragment: Binding is null")
 
     private var _binding: FragmentListCategoriesBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/ru/paulgri/ra1app/FavoritesFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/FavoritesFragment.kt
@@ -1,12 +1,11 @@
 package ru.paulgri.ra1app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.paulgri.ra1app.databinding.FragmentFavoritesBinding
-import ru.paulgri.ra1app.databinding.FragmentListCategoriesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,22 +18,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FavoritesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    val binding: FragmentFavoritesBinding
+    private val binding: FragmentFavoritesBinding
         get() = _binding ?: throw IllegalStateException("CategoriesListFragment: Binding is null")
 
     private var _binding: FragmentFavoritesBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
