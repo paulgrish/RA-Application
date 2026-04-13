f994aa2bf68b71e03ee78e35a3fbfdd2eb151482
paulgri <paulgrishmanovsky@gmail.com>
2026-04-14 00:24:50 +0500

    RA-05 fix2

--- app/src/main/java/ru/paulgri/ra1app/CategoriesListFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/CategoriesListFragment.kt
@@ -7,11 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.paulgri.ra1app.databinding.FragmentListCategoriesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [CategoriesListFragment.newInstance] factory method to
@@ -36,24 +31,5 @@ class CategoriesListFragment : Fragment() {
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
-    }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoriesListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoriesListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
