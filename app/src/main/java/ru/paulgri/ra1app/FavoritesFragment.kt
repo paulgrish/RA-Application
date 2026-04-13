201ac66d6c07090ab23be2abaa244b2b29ba88ca
paulgri <paulgrishmanovsky@gmail.com>
2026-04-13 23:53:20 +0500

    RA-05 done

--- app/src/main/java/ru/paulgri/ra1app/FavoritesFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/FavoritesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.paulgri.ra1app.databinding.FragmentFavoritesBinding
+import ru.paulgri.ra1app.databinding.FragmentListCategoriesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,11 @@ class FavoritesFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    val binding: FragmentFavoritesBinding
+        get() = _binding ?: throw IllegalStateException("CategoriesListFragment: Binding is null")
+
+    private var _binding: FragmentFavoritesBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,9 +39,16 @@ class FavoritesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 
     companion object {
