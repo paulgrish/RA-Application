771b8bb47fde9b7caaa7866b253a38a527ad5729
paulgri <paulgrishmanovsky@gmail.com>
2026-04-19 20:30:40 +0500

    RA-06 data classes

---
+++ app/src/main/java/ru/paulgri/ra1app/dataclasses.kt
@@ -0,0 +1,20 @@
+package ru.paulgri.ra1app
+
+data class Categories(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+)
+
+data class Ingredient(
+    val quantity: Float,
+    val unitOfMeasure: String,
+    val description: String,
+)
+
+data class Recipe(
+    val id: Int,
+    val title: String,
+    val ingredients: List<Ingredient>,
+)
