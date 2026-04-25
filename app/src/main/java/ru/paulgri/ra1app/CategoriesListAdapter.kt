6588b1a9bc71cc0b32b69e6903d9d82fa8d1c937
paulgri <paulgrishmanovsky@gmail.com>
2026-04-25 14:10:37 +0500

    RA-08 adapter

---
+++ app/src/main/java/ru/paulgri/ra1app/CategoriesListAdapter.kt
@@ -0,0 +1,54 @@
+package ru.paulgri.ra1app
+
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.paulgri.ra1app.databinding.ItemCategoryBinding.bind
+
+class CategoriesListAdapter(private val dataSet: List<Category>) :
+    RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val binding = bind(itemView)
+        val ivImage = binding.ivCardImage
+        val tvTitle = binding.tvCardTitle
+        val tvDescription = binding.tvCardDescription
+
+        init {
+            // Define click listener for the ViewHolder's View
+//            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_category, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        try {
+            viewHolder.tvTitle.text = dataSet[position].title
+            viewHolder.tvDescription.text = dataSet[position].description
+            viewHolder.ivImage.setImageDrawable(
+                Drawable.createFromStream(
+                    viewHolder.itemView.context.assets.open(dataSet[position].imageUrl),
+                    null
+                )
+            )
+        } catch (e: Exception) {
+            Log.println(Log.ERROR, "CategoryListAdapter", e.stackTrace.toString())
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
