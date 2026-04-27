e7386cbc1b1fd597f1a87afa38d36d41bb93537d
paulgri <paulgrishmanovsky@gmail.com>
2026-04-27 22:17:04 +0500

    RA-09 fix2

--- app/src/main/java/ru/paulgri/ra1app/RecipesListFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/RecipesListFragment.kt
@@ -8,15 +8,10 @@ import android.view.ViewGroup
 
 class RecipesListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipes_list, container, false)
     }
 }
