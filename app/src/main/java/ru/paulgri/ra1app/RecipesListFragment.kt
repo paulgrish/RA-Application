7ad9b6859c1f3abdc2bafc9346e8b452b5e0ec81
paulgri <paulgrishmanovsky@gmail.com>
2026-05-01 11:04:20 +0500

    RA-11 header

--- app/src/main/java/ru/paulgri/ra1app/RecipesListFragment.kt
+++ app/src/main/java/ru/paulgri/ra1app/RecipesListFragment.kt
@@ -1,12 +1,19 @@
 package ru.paulgri.ra1app
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.paulgri.ra1app.databinding.FragmentRecipesListBinding
 
 class RecipesListFragment : Fragment() {
+
+    private val binding: FragmentRecipesListBinding
+        get() = _binding ?: throw IllegalStateException("RecipesListFragment: Binding is null")
+    private var _binding: FragmentRecipesListBinding? = null
 
     var categoryId: Int? = null
     var categoryName: String? = null
@@ -16,13 +23,29 @@ class RecipesListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recipes_list, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentRecipesListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         categoryId = arguments?.getInt(ARG_CATEGORY_ID)
         categoryName = arguments?.getString(ARG_CATEGORY_NAME)
         categoryImageUrl = arguments?.getString(ARG_CATEGORY_IMAGE_URL)
+
+        binding.tvHeaderTitle.text = categoryName
+        binding.ivHeaderImage.setImageDrawable(
+            try {
+                Drawable.createFromStream(
+                    binding.ivHeaderImage.context.assets.open(categoryImageUrl ?: ""),
+                    null
+                )
+            } catch (e: Exception) {
+                Log.e("RecipesListFragment", e.stackTrace.toString())
+                null
+            }
+        )
     }
 }
