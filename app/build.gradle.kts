35f5bbec8e77062278c3d0a749cae3aafefb37cd
paulgri <paulgrishmanovsky@gmail.com>
2026-05-18 20:31:42 +0500

    RA-12 fix2 plugin & kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("org.jetbrains.kotlin.plugin.parcelize")
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -42,7 +42,6 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-parcelize-runtime:1.9.20")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/ru/paulgri/ra1app/dataclasses.kt
+++ app/src/main/java/ru/paulgri/ra1app/dataclasses.kt
@@ -1,110 +1,28 @@
 package ru.paulgri.ra1app
 
-import android.os.Build
-import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-//@Parcelize
+@Parcelize
 data class Category(
     val id: Int,
     val title: String,
     val description: String,
     val imageUrl: String,
-) : Parcelable {
-    override fun describeContents(): Int {
-        return 0
-    }
+) : Parcelable
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(description)
-        parcel.writeString(imageUrl)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Category> {
-        override fun createFromParcel(parcel: Parcel): Category? {
-            return Category(
-                parcel.readInt(),
-                parcel.readString().toString(),
-                parcel.readString().toString(),
-                parcel.readString().toString(),
-            )
-        }
-
-        override fun newArray(size: Int): Array<out Category?>? {
-            return arrayOfNulls(size)
-        }
-    }
-}
-
-//@Parcelize
+@Parcelize
 data class Ingredient(
     val quantity: Float,
     val unitOfMeasure: String,
     val description: String,
-) : Parcelable {
-    override fun describeContents(): Int {
-        return 0
-    }
+) : Parcelable
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeFloat(quantity)
-        parcel.writeString(unitOfMeasure)
-        parcel.writeString(description)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Ingredient> {
-        override fun createFromParcel(parcel: Parcel): Ingredient? {
-            return Ingredient(
-                parcel.readFloat(),
-                parcel.readString().toString(),
-                parcel.readString().toString(),
-            )
-        }
-
-        override fun newArray(size: Int): Array<out Ingredient?>? {
-            return arrayOfNulls(size)
-        }
-
-    }
-}
-
-//@Parcelize
+@Parcelize
 data class Recipe(
     val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
     val method: List<String>,
     val imageUrl: String,
-) : Parcelable {
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeParcelableList(ingredients, flags)
-        parcel.writeStringList(method)
-        parcel.writeString(imageUrl)
-    }
-
-    companion object CREATOR : Parcelable.Creator<Recipe> {
-        override fun createFromParcel(parcel: Parcel): Recipe? {
-            return Recipe(
-                parcel.readInt(),
-                parcel.readString().toString(),
-                @Suppress("DEPRECATION") parcel.readParcelableList(mutableListOf(), Ingredient::class.java.classLoader),
-                parcel.createStringArrayList() ?: emptyList(),
-                parcel.readString().toString(),
-            )
-        }
-
-        override fun newArray(size: Int): Array<out Recipe?>? {
-            return arrayOfNulls(size)
-        }
-
-    }
-}
+) : Parcelable
